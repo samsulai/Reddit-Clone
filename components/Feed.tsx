1609cfa9ffac6fe9b131b7ac3a12587b2e088519
samsulai <samuelsulai2015@gmail.com>
2022-07-26 18:47:23 +0100

    bug fix

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
 import { Waveform } from '@uiball/loaders'
-import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC, GET } from '../graphql/queries'
+import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC,  } from '../graphql/queries'
 import Post from './Post'
 
 type Props  = {
