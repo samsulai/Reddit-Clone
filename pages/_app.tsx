10d177f0ac6771dc52d90fad2aeb3e71e4b2cb61
samsulai <samuelsulai2015@gmail.com>
2022-07-26 19:20:53 +0100

    added all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
-import header from '../components/header'
+import Header from '../components/Header'
 import {ApolloProvider} from '@apollo/client'
 import client from '../apollo-client'
 import {Toaster} from 'react-hot-toast'
